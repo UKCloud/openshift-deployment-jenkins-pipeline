34166735349d5861c52f879d29d84e42758c3a05
Cloud User <cloud-user@bastion-host.localdomain>
2017-07-05 09:35:50 -0400

    Move to templates

--- setup-ci.sh
+++ setup-ci.sh
@@ -3,22 +3,19 @@
 echo 'Please enter the password for the openstack CI user (openshift@ukcloud.com)'
 read password
 
-NAME='openstack-jenkins-slave'
-SOURCE_REPOSITORY_URL='https://github.com/charliejllewellyn/openshift-jenkins-pipeline.git'
+NAME='openshift-build-pipeline'
+SOURCE_REPOSITORY_URL='https://github.com/UKCloud/openshift-deployment-jenkins-pipeline.git'
 SOURCE_REPOSITORY_REF='master'
-CONTEXT_DIR='docker/openstack-jenkins-slave/'
-PIPELINE_CONTEXT_DIR='jenkins-pipelines/openstack/'
+CONTEXT_DIR='jenkins-pipelines/openshift'
 
 
 function setup_openshift_deployment_jenkins_pipeline() {
 
-    #oc new-app -f openshift-yaml/template-openstackclient-jenkins-slave.yaml \
-    #    -p NAME=$NAME \
-    #    -p SOURCE_REPOSITORY_URL=$SOURCE_REPOSITORY_URL \
-    #    -p SOURCE_REPOSITORY_REF=$SOURCE_REPOSITORY_REF \
-    #    -p CONTEXT_DIR=$CONTEXT_DIR \
-    #    -p PIPELINE_CONTEXT_DIR=$PIPELINE_CONTEXT_DIR
-    oc new-build https://github.com/UKCloud/openshift-deployment-jenkins-pipeline.git --context-dir=jenkins-pipelines/openshift/ --name=openshift-build
+    oc new-app -f openshift-yaml/template-openshift-build.yaml \
+        -p NAME=$NAME \
+        -p SOURCE_REPOSITORY_URL=$SOURCE_REPOSITORY_URL \
+        -p SOURCE_REPOSITORY_REF=$SOURCE_REPOSITORY_REF \
+        -p CONTEXT_DIR=$CONTEXT_DIR
 }
 
 function setup_openstack_variables() {
@@ -29,10 +26,10 @@ function setup_openstack_variables() {
 
 function configure_openshift_githooks() {
     # TODO: Automate webhook creation and updates via the github API
-    gitHook=$(oc describe bc ${NAME}-pipeline | grep -A1 'Webhook GitHub' | grep URL | awk '{print $NF}')
+    gitHook=$(oc describe bc ${NAME} | grep -A1 'Webhook GitHub' | grep URL | awk '{print $NF}')
     echo "Add a github webhook for the following URL to trigger automated builds of the Jenkins pipeline, $gitHook"
 }
 
-#setup_openshift_deployment_jenkins_pipeline
+setup_openshift_deployment_jenkins_pipeline
 setup_openstack_variables
 configure_openshift_githooks
